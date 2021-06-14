375b7c0bb3a06fdf99f0663619f137b47830cbad
Goldi <Goldilocks.13.a@gmail.com>
2021-06-14 22:17:54 +0400

    что-то нечто

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from random import randint
-
 import requests
-import updater
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup, update
 from aiogram.utils import executor
 
 from config import TOKEN
@@ -20,25 +17,21 @@ db = DBManager()
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Кушать", "Случайный", "Локация", "Помощь"]
+    buttons = ["Категории", "Любое", "Помощь"]
+    name = message.from_user.first_name
     keyboard.add(*buttons)
-    await message.answer(
-        "Привет!\n" \
-        "Я помогу тебе найти ресторан на сегодня.\n" \
-        "Чтобы начать напиши: Кушать", \
-        reply_markup=keyboard)
+    await message.answer(f'Привет {name}!\nЯ помогу тебе найти ресторан на сегодня\nЧтобы начать, напиши: Категории', reply_markup=keyboard)
 
 
 @dp.message_handler(lambda message: message.text == "Помощь")
 async def process_help_command(message: types.Message):
     await message.reply(
-        "Помогите\n" \
-        "пожалуйста помогите.\n" \
-        "Чтобы начать напиши: Кушать" \
+        "Чтобы сделать выбор из категрии, нажмите на кнопку: Категории, или введите: Категории\n" \
+        "Чтобы выбрать случайный ресторан нажмите на кнопку: Случайный, или введите: Случайный.\n" \
         )
 
 
-@dp.message_handler(lambda msg: msg.text == "Кушать")
+@dp.message_handler(lambda msg: msg.text == "Категории")
 async def get_categories_command(msg: types.Message):
     """Получение всех категорий"""
     categories = db.get_categories()
@@ -55,7 +48,7 @@ async def get_categories_command(msg: types.Message):
     await bot.send_message(msg.from_user.id, 'Категории:', reply_markup=keyboard)
 
 
-@dp.message_handler(lambda msg: msg.text == "Случайный")
+@dp.message_handler(lambda msg: msg.text == "Любое")
 async def get_product_random(msg: types.Message):
     """Вывод рандомного ресторана"""
     randoms = db.get_product_random()
@@ -129,7 +122,7 @@ async def get_products_callback(callback_query: types.CallbackQuery):
 
 @dp.callback_query_handler(lambda call: call.data and call.data.startswith('rand_'))
 async def get_products_callback(callback_query: types.CallbackQuery):
-    """ЕЕЕЕЕЕЕЕЕЕЕЕЕМООООООООООООООООООЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕ"""
+    """Вывод случайного ресторана"""
     query = callback_query.data.replace('rand_', '')  # Убрать пометку callback'ов
     product = db.get_product(query)
     keyboard = types.InlineKeyboardMarkup()
@@ -183,46 +176,6 @@ async def get_products_callback(callback_query: types.CallbackQuery):
         reviews_str += review_str
 
     await bot.send_message(callback_query.from_user.id, reviews_str, parse_mode='markdown')
-
-
-"""aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanтсюда"""
-
-
-@dp.callback_query_handler(lambda msg: msg.text == "Локация")
-def get_address_from_coords(update, context):
-    # получаем обьект сообщения (локации)
-    message = update.message
-    # вытаскиваем из него долготу и ширину
-    current_position = (message.location.longitude, message.location.latitude)
-    # создаем строку в виде ДОЛГОТА,ШИРИНА
-    coords = f"{current_position[0]},{current_position[1]}"
-    # отправляем координаты в нашу функцию получения адреса
-    address_str = get_address_from_coords(coords)
-    # вовщращаем результат пользователю в боте
-    update.message.reply_text(address_str)
-
-    PARAMS = {
-        "apikey": "ваш api key",
-        "format": "json",
-        "lang": "ru_RU",
-        "kind": "house",
-        "geocode": coords
-    }
-
-    # отправляем запрос по адресу геокодера.
-    try:
-        r = requests.get(url="https://geocode-maps.yandex.ru/1.x/", params=PARAMS)
-        json_data = r.json()
-        address_str = json_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["metaDataProperty"][
-            "GeocoderMetaData"]["AddressDetails"]["Country"]["AddressLine"]
-        return address_str
-
-    except Exception as e:
-        # единственное что тут изменилось, так это сообщение об ошибке.
-        return "Не могу определить адрес по этой локации/координатам.\n\nОтправь мне локацию или координаты (долгота, широта):"
-
-    # Эта функция будет использоваться, если пользователь послал локацию.
-
 
 
 if __name__ == '__main__':
